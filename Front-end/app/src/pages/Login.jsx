becca3d99ddd0ce580007eaeec3fd4277527e17b
Dhieu <dhieumajok211@gmail.com>
2024-10-16 20:47:00 +0300

    added login & Sigup fuctionality

--- Front-end/app/src/pages/Login.jsx
+++ Front-end/app/src/pages/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const response = await axios.post('http://127.0.0.1:8000/api/token/', {
+      const response = await axios.post('http://127.0.0.1:8000/token/', {
         username,
         password
       });
