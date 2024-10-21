25a32d0142f2c9cd579f4519793eebb1b47047b1
Dhieu <dhieumajok211@gmail.com>
2024-10-21 07:55:17 +0300

    add a new accounts

--- Front-end/app/src/pages/About.jsx
+++ Front-end/app/src/pages/About.jsx
@@ -20,7 +20,7 @@ const About = () => {
 
       <div class="flex justify-center h-screen mt-4">
         <div class="relative group">
-          <Link to="/home" class="no-underline relative inline-block p-px font-semibold leading-6 text-white bg-gray-800 shadow-2xl cursor-pointer rounded-xl shadow-zinc-900 transition-transform duration-300 ease-in-out hover:scale-105 active:scale-95">
+          <Link to="/" class="no-underline relative inline-block p-px font-semibold leading-6 text-white bg-gray-800 shadow-2xl cursor-pointer rounded-xl shadow-zinc-900 transition-transform duration-300 ease-in-out hover:scale-105 active:scale-95">
             <span class="absolute inset-0 rounded-xl bg-gradient-to-r from-teal-400 via-blue-500 to-purple-500 p-[2px] opacity-0 transition-opacity duration-500 group-hover:opacity-100"></span>
 
             <span class="relative z-10 block px-6 py-3 rounded-xl bg-gray-750">
