7fc2cdc1fd9880cf7f4e861adb34b10d731e4902
Dhieu <dhieumajok211@gmail.com>
2024-10-21 08:08:00 +0300

    add some spaces in dashboard page

--- Front-end/app/src/pages/Dashboard.jsx
+++ Front-end/app/src/pages/Dashboard.jsx
@@ -5,7 +5,8 @@ const Dashboard = () => {
     return (
         <div className="container mt-5">
             <h1>Welcome to ProjeXplore.</h1>
-            <p>This is your Dashboard. Enjoy your stay!</p> <br></br><br></br><br></br>
+            <p>This is your Dashboard. Enjoy your stay!</p> <br></br><br></br><br></br><br></br><br></br><br></br>
+            <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
         </div>
     );
 };
